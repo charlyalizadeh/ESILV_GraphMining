448298db912edc5baba8fb665a570d5ab3a3f8e5
charlyalizadeh <charlypro1502@outlook.fr>
2022-01-08 20:32:08 +0100

    Modify bi partite graph from Article/Category to Article/Path

--- src/build_dataset.py
+++ src/build_dataset.py
@@ -3,14 +3,6 @@ from urllib.parse import unquote
 from collections import defaultdict
 
 
-# Monopartite
-# ====NODE====
-# Article Name, Subjects (delimited by a `|` categorypaths and a `.` for the categories), ID (to create)
-# ====LINK===
-# ID_from, ID_to, r:WIKILINK
-# ====PATH====
-# * add relationship using the distance matrix (maybe too long, 4616 node, so 4616 * 4615 link = 21 302 840)
-
 def get_categories_dict(path):
     categories = defaultdict(list)
     with open(path, 'r') as categories_file:
@@ -39,24 +31,25 @@ def build_mono_partite_nodes(return_article2id=True):
     categories = get_categories_dict('data/wikispeedia_paths-and-graph/categories.tsv')
     rows = {
         'id': [],
-        'article': [],
-        'article_decoded': [],
+        'name': [],
+        'name_decoded': [],
         'categories': []
     }
     with open('data/wikispeedia_paths-and-graph/articles.tsv', 'r') as article_file:
         for i, article in enumerate(article_file.readlines()[12:]):
             article = article.strip()
             rows['id'].append(i)
-            rows['article'].append(article)
-            rows['article_decoded'].append(unquote(article))
-            cat = '|'.join(categories[article])
+            rows['name'].append(article)
+            rows['name_decoded'].append(unquote(article))
+            cat = '.'.join(categories[article])
             cat = cat if not all([c == ' ' for c in cat]) else "NaN"
             rows['categories'].append(cat)
 
     df = pd.DataFrame(data=rows)
-    df.to_csv('data/mono_partite/nodes.csv', index=False, sep=',')
+    df.to_csv('data/mono_partite/articles.csv', index=False, sep=',')
+    df.to_csv('data/bi_partite/articles.csv', index=False, sep=',')
     if return_article2id:
-        return dict(zip(rows['article'], rows['id']))
+        return dict(zip(rows['name'], rows['id']))
 
 
 def build_mono_partite_relationship(article2id):
@@ -74,7 +67,7 @@ def build_mono_partite_relationship(article2id):
     df.to_csv('data/mono_partite/relationships.csv', index=False, sep=',')
 
 
-def build_bi_partite_graph():
+def build_bi_partite_graph_article_category():
     articles = get_articles_list('data/wikispeedia_paths-and-graph/articles.tsv')
     categories = get_categories_list('data/wikispeedia_paths-and-graph/categories.tsv')
     categories_dict = get_categories_dict('data/wikispeedia_paths-and-graph/categories.tsv')
@@ -103,11 +96,63 @@ def build_bi_partite_graph():
             relationships['id_from'].append(articles2id[k])
             relationships['id_to'].append(categories2id[c])
     df_relationships = pd.DataFrame(data=relationships)
-    df_articles.to_csv('data/bi_partite/articles.csv', index=False, sep=',')
-    df_categories.to_csv('data/bi_partite/categories.csv', index=False, sep=',')
-    df_relationships.to_csv('data/bi_partite/relationships.csv', index=False, sep=',')
+    df_articles.to_csv('data/bi_partite_article_category/articles.csv', index=False, sep=',')
+    df_categories.to_csv('data/bi_partite_article_category/categories.csv', index=False, sep=',')
+    df_relationships.to_csv('data/bi_partite_article_category/relationships.csv', index=False, sep=',')
+
+
+def build_bi_partite_graph_article_paths(article2id):
+    # Paths
+    paths = {
+            'id': [],
+            'timestamp': [],
+            'durationInSec': [],
+            'path': [],
+            'rating': [],
+            'state': [],
+            'target': []
+    }
+    start_at = max([article2id[k] for k in article2id.keys()])
+    with open('data/wikispeedia_paths-and-graph/paths_finished.tsv') as path_file:
+        for i, line in enumerate(path_file.readlines()[16:]):
+            _id, timestamp, duration, path, rating = line.strip().split('\t')
+            paths['id'].append(start_at + i)
+            paths['timestamp'].append(timestamp)
+            paths['durationInSec'].append(duration)
+            paths['path'].append(path)
+            paths['rating'].append(rating)
+            paths['state'].append('finished')
+            paths['target'].append(path.split(';')[-1])
+
+    start_at = max(paths['id'])
+    with open('data/wikispeedia_paths-and-graph/paths_unfinished.tsv') as path_file:
+        for i, line in enumerate(path_file.readlines()[17:]):
+            _id, timestamp, duration, path, target, state = line.strip().split('\t')
+            paths['id'].append(start_at + i)
+            paths['timestamp'].append(timestamp)
+            paths['durationInSec'].append(duration)
+            paths['path'].append(path)
+            paths['rating'].append('UNFINISHED')
+            paths['state'].append(state)
+            paths['target'].append(target)
+
+    df = pd.DataFrame(data=paths)
+    df.to_csv('data/bi_partite/paths.csv', index=False, sep=',')
+
+    is_in = {
+            'id_from': [],
+            'id_to': []
+    }
+    for _id, path, target in zip(paths['id'], paths['path'], paths['target']):
+        for article in path.split(';'):
+            if article == '<':
+                continue
+            is_in['id_from'].append(article2id[article])
+            is_in['id_to'].append(_id)
+
+    pd.DataFrame(data=is_in).to_csv('data/bi_partite/is_in.csv', index=False, sep=',')
 
 
 article2id = build_mono_partite_nodes()
 build_mono_partite_relationship(article2id)
-build_bi_partite_graph()
+build_bi_partite_graph_article_paths(article2id)
--- src/load_database.cypher
+++ src/load_database.cypher
@@ -1,6 +1,6 @@
 // Mono partite nodes
 LOAD CSV WITH HEADERS FROM 'file:///nodes.csv' AS row
-MERGE (a:Article{id:row.id, name:row.name, name_decoded:row.name_decoded, categories:split(row.categories, "|")});
+MERGE (a:Article{id:row.id, name:row.name, name_decoded:row.name_decoded, categories:split(row.categories, ".")});
 CREATE CONSTRAINT articleIdConstraint FOR (a:Article) REQUIRE a.id IS UNIQUE;
 // Mono partite relationships
 LOAD CSV WITH HEADERS FROM "file:///relationships.csv" AS row
@@ -8,7 +8,8 @@ MATCH (a1:Article{id:row.id_from}), (a2:Article{id:row.id_to})
 CREATE (a1)-[:LINK]->(a2);
 
 
-// Bi partite nodes
+// ARTICLE/CATEGORY
+// Bi partite nodes 
 // Articles
 LOAD CSV WITH HEADERS FROM 'file:///articles.csv' AS row
 MERGE (a:Article{id:row.id, name:row.name, name_decoded:row.name_decoded});
@@ -17,7 +18,31 @@ LOAD CSV WITH HEADERS FROM 'file:///categories.csv' AS row
 MERGE (a:Category{id:row.id, name:row.name, subcategories:split(row.category, '.')});
 CREATE CONSTRAINT articleIdConstraint FOR (n:Article) REQUIRE n.id IS UNIQUE;
 CREATE CONSTRAINT categoryIdConstraint FOR (n:Category) REQUIRE n.id IS UNIQUE;
+
 // Bi partite relationships
 LOAD CSV WITH HEADERS FROM "file:///relationships.csv" AS row
 MATCH (a:Article{id:row.id_from}), (c:Category{id:row.id_to})
 CREATE (a)-[:CATEGORY]->(c);
+
+
+// ARTICLE/PATH
+// Articles
+LOAD CSV WITH HEADERS FROM 'file:///articles.csv' AS row
+MERGE (a:Article{id:row.id, name:row.name, name_decoded:row.name_decoded, categories:split(row.categories, ".")});
+// Paths
+LOAD CSV WITH HEADERS FROM 'file:///paths.csv' AS row
+MERGE (p:Path{
+            id:row.id,
+            timestamp:row.timestamp,
+            duration:row.durationInSec,
+            path:split(row.path, ';'),
+            rating:row.rating,
+            state:row.state,
+            target:row.target
+});
+CREATE CONSTRAINT articleIdConstraint FOR (n:Article) REQUIRE n.id IS UNIQUE;
+CREATE CONSTRAINT pathIdConstraint FOR (n:Path) REQUIRE n.id IS UNIQUE;
+// Relationships
+LOAD CSV WITH HEADERS FROM 'file:///relationships.csv' AS row
+MATCH (a:Article{id:row.id_from}), (p:Path{id:row.id_to})
+CREATE (a)-[:IS_IN]->(p);
